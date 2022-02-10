18d3e031af64b4d92833272f4f017599b3ac0a7c
James Rozsypal <jimmerxl@gmail.com>
2022-02-10 12:23:56 -0500

    Add header and footer

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const DEFAULT_GRID = 16;
 
 const body = document.querySelector("body");
 const container = document.createElement("div");
+const header = document.createElement("div");
 const grid = document.createElement("div");
+const footer = document.createElement("div");
 
 container.classList.add("container");
 grid.classList.add("grid");
@@ -15,8 +17,26 @@ let getGrid = (size) => {
   }
 };
 
+let getHeader = () => {
+  header.classList.add("header");
+  const title = document.createElement("h1");
+  title.textContent = "ETCH-A-SKETCH";
+  header.appendChild(title);
+  container.appendChild(header);
+};
+
+let getFooter = () => {
+  footer.classList.add("footer");
+  const credit = document.createElement("p");
+  credit.textContent = "James Rozsypal 2022";
+  footer.appendChild(credit);
+  container.appendChild(footer);
+};
+
 window.onload = () => {
   getGrid(DEFAULT_GRID);
+  getHeader();
   container.appendChild(grid);
+  getFooter();
   body.appendChild(container);
 };
