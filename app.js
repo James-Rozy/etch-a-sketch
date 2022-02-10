eeef14b6b5256ea6cf4ef8c38a8ab59c824cb472
James Rozsypal <jimmerxl@gmail.com>
2022-02-10 14:31:33 -0500

    Clean up code by outlining structure in html and filling grid in js

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 const DEFAULT_GRID = 16;
 
-const body = document.querySelector("body");
-const container = document.createElement("div");
-const header = document.createElement("div");
-const grid = document.createElement("div");
-const footer = document.createElement("div");
-
-container.classList.add("container");
-grid.classList.add("grid");
+const grid = document.querySelector(".grid");
 
 let getGrid = (size) => {
   for (let i = 0; i < size * size; i++) {
@@ -17,26 +10,6 @@ let getGrid = (size) => {
   }
 };
 
-let getHeader = () => {
-  header.classList.add("header");
-  const title = document.createElement("h1");
-  title.textContent = "ETCH-A-SKETCH";
-  header.appendChild(title);
-  container.appendChild(header);
-};
-
-let getFooter = () => {
-  footer.classList.add("footer");
-  const credit = document.createElement("p");
-  credit.textContent = "James Rozsypal 2022";
-  footer.appendChild(credit);
-  container.appendChild(footer);
-};
-
 window.onload = () => {
   getGrid(DEFAULT_GRID);
-  getHeader();
-  container.appendChild(grid);
-  getFooter();
-  body.appendChild(container);
 };
