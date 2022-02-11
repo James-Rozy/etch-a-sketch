53eb7b50769eebabe52b7a4ba950d001c2e6e0c8
James Rozsypal <jimmerxl@gmail.com>
2022-02-11 12:43:54 -0500

    Finish basic ui

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const DEFAULT_GRID = 16;
 const DEFAULT_COLOR = "#424949";
+
+let penColor = DEFAULT_COLOR;
+let gridSize = DEFAULT_GRID;
+
 const grid = document.querySelector(".grid");
+const btnReset = document.querySelector(".reset");
+const inpColor = document.querySelector("#color");
+const inpSlider = document.querySelector("#sizeSlider");
+const txtSize = document.querySelector("#size");
+
+btnReset.onclick = () => newGrid();
+inpColor.onchange = (e) => setColor(e.target.value);
+inpSlider.onmousemove = (e) => setSlideSize(e.target.value);
+inpSlider.onchange = (e) => setSize(e.target.value);
 
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
@@ -9,7 +22,7 @@ document.body.onmouseup = () => (mouseDown = false);
 let getGrid = (size) => {
   grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-  
+
   for (let i = 0; i < size * size; i++) {
     const pixel = document.createElement("div");
     pixel.classList.add("pixel");
@@ -24,8 +37,30 @@ let etchSketch = (e) => {
     return;
   }
 
-  e.target.style.backgroundColor = `${DEFAULT_COLOR}`;
+  e.target.style.backgroundColor = `${penColor}`;
 };
+
+let newGrid = () => {
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+  }
+
+  getGrid(gridSize);
+};
+
+let setColor = (color) => {
+  penColor = color;
+} 
+
+let setSlideSize = (size) => {
+  txtSize.textContent = size + "X" + size;
+}
+
+let setSize = (size) => {
+  gridSize = size;
+  setSlideSize(size);
+  newGrid();
+}
 
 window.onload = () => {
   getGrid(DEFAULT_GRID);
